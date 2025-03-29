79022cf5e56589283acef8f10f83a04c3ba00dac
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2025-03-29 22:00:53 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/supabase/riverpod/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.supabase_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:supabase_riverpod/app/app.dart';
+import 'package:supabase_riverpod/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
