53addeb484baa4758aec6321defe5417a7030b03
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2025-03-29 22:39:10 +0100

    Added DOTENV package

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_riverpod/app/app.dart';
 import 'package:supabase_riverpod/bootstrap.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<void> main() async {
+  await dotenv.load();
+  await bootstrap(() => const App());
 }
