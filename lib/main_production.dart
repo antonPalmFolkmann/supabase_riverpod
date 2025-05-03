3b90f93e6a7afa3653969b5250f88ce05c2e34c2
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2025-05-03 13:45:26 +0200

    Initilize supabase

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:supabase_riverpod/app/app.dart';
 
 Future<void> main() async {
   await dotenv.load();
+  final supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  final supabaseKey = dotenv.env['SUPABASE_KEY'] ?? '';
+  await Supabase.initialize(url: supabaseUrl,anonKey: supabaseKey);
   runApp(const ProviderScope(child: App()));
 }
